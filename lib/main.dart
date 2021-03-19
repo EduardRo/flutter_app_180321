1c9450aef52dc75b9f1a5e6c8966cfffcd119451
EduardRo <eduardstancu@yahoo.com>
2021-03-20 00:18:21 +0200

    modifications to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello darkness my old friend!'));
+  }
+}
