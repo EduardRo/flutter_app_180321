00b68d2e09937eeee0a7019c06f8ac3d8146511d
EduardRo <eduardstancu@yahoo.com>
2021-03-20 12:26:23 +0200

    create buttons in the widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,35 @@ void main() {
   runApp(MyApp());
 }
 
+// Alternative syntac for the main function
+// void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer choosen');
+  }
+
   @override
+  // @override is a declarator - we override the method of the class
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello darkness my old friend!'));
+    var question = ['question 1', 'question 2'];
+
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('My App')),
+            body: Column(children: [
+              Text('Text for body'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () => print('button3'),
+              ),
+            ])));
   }
 }
